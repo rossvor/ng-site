8078f5f98ea17f8c26cc4c5b05ca21a6c86c4a23
Ross Vorotynskij <ross@rvcg.net>
2014-05-11 10:37:04 +0100

    Added shades implementation

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,14 +2,32 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+angular.module('myApp.controllers', ['myApp.services'])
+  .controller('MyCtrl1', ['$scope', 'sidesShader', function($scope, shaderService) {
 	  
-	  var centre = {x:500, y:500};
+	  var center = {x:500, y:500};
+	  
+	  //default opacity
+	  $scope.eastside = 0.001;
+	  $scope.northside = 0.5;
+	  $scope.westside = 0.8;
+	  $scope.southside = 0.9;
+	  
+	  $scope.angle = 0;
 	  
 	  $scope.moved = function(e) {
-		$scope.xPos = e.pageX;
-		$scope.yPos = e.pageY;
+		
+		var x = e.pageX;
+		var y = e.pageY;
+		$scope.xPos = x;
+		$scope.yPos = y;
+		
+		var newOpacity =  shaderService.calculateOpacity(x,y, center);
+		$scope.eastside = newOpacity.eastside;
+		$scope.northside = newOpacity.northside;
+		$scope.westside = newOpacity.westside;
+		$scope.southside = newOpacity.southside;
+		$scope.angle = newOpacity.angle;
 	}
 
   }])
--- app/js/services.js
+++ app/js/services.js
@@ -9,25 +9,39 @@ angular.module('myApp.services', [])
   .value('version', '0.1')
 
   .service('sidesShader', function() {
-	  this.calculateColors = function( px, py, center) {
+	  // this one is public static method
+	  this.calculateOpacity = function( px, py, center) {
 		  // offset the coordinates
-		  var x = px - center.x;
-		  var y = py - center.y;	 
+		  var offsetResult = this.offset(px,py, center.x, center.y);
+		  var x = offsetResult.x;
+		  var y = offsetResult.y;	 
 
 		  var EAST = 0;
-		  var NORTH = 1.5707963267948966;
+		  var NORTH = -1.5707963267948966;
 		  var WEST = 3.141592653589793;
-		  var SOUTH = -1.5707963267948966;
+		  var SOUTH = 1.5707963267948966;
+		  
+		  Math.Pi;
+		  
+		  var angleInRads = Math.atan2(y, x);
 
+		  return { eastside:this.opacityByRadians( angleInRads),
+			  	  northside:this.opacityByRadians( Math.abs(Math.atan2(x, y)) - Math.PI),
+			  	  westside:this.opacityByRadians( Math.abs(angleInRads) - Math.PI),
+			  	  southside:this.opacityByRadians( Math.abs(Math.atan2(x, y))),
+			  	  
+			  	  angle:angleInRads
+			  	};
 
-		  e: 0
-		  n: 1.5707963267948966
-		  w: 3.141592653589793
-		  s: -1.5707963267948966
 	  }
 	  
 	  this.offset = function(x, y, cx, cy) {
 		return {x:x-cx , y:y-cy };
+	}
+	  this.opacityByRadians = function(radian) {
+		  
+		// Biggest difference is PI
+		return Math.abs(radian) / Math.PI;
 	}
 	  
 	  
